92ca07aa2f2b53de9f783a77dfc31584238c674d
Bruce-zxy <bruce_zxy@163.com>
2018-06-13 21:30:20 +0800

    desktop for dl has been finished

--- dl/assets/js/init-datatables.js
+++ dl/assets/js/init-datatables.js
@@ -1,4 +1,3 @@
-
 function format(d) {
     // `d` is the original data object for the row
     return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
@@ -22,7 +21,7 @@ function format(d) {
 
 $('.convert-data-table').DataTable({
     "PaginationType": "bootstrap",
-    dom: '<"tbl-head clearfix"T>,<"tbl-top clearfix"lfr>,t,<"tbl-footer clearfix"<"tbl-info pull-left"i><"tbl-pagin pull-right"p>>',
+    dom: '<"tbl-head clearfix"T>,<"tbl-top clearfix"lfr>,t,<"tbl-footer clearfix"<"tbl-info pull-left"i>>',
     tableTools: {
         "sSwfPath": "swf/copy_csv_xls_pdf.swf"
     }
@@ -33,10 +32,95 @@ $('.convert-data-table').DataTable({
 
 $('.colvis-data-table').DataTable({
     "PaginationType": "bootstrap",
-    dom: '<"tbl-head clearfix"C>,<"tbl-top clearfix"lfr>,t,<"tbl-footer clearfix"<"tbl-info pull-left"i><"tbl-pagin pull-right"p>>'
-
-
+    dom: '<"tbl-head clearfix"T>'
 });
+
+// $('.colvis-data-table').DataTable({
+//     "dom": '', //dom定位
+//     "dom": '', //自定义显示项
+//     //"dom":'<"domab"f>',
+//     "scrollY": "220px", //dt高度
+//     "lengthMenu": [
+//         [8, 6, 8, -1],
+//         [4, 6, 8, "All"]
+//     ], //每页显示条数设置
+//     "lengthChange": false, //是否允许用户自定义显示数量
+//     "bPaginate": true, //翻页功能
+//     "bFilter": false, //列筛序功能
+//     "searching": true, //本地搜索
+//     "ordering": true, //排序功能
+//     "Info": true, //页脚信息
+//     "autoWidth": true, //自动宽度
+//     "oLanguage": { //国际语言转化
+//         "oAria": {
+//             "sSortAscending": " - click/return to sort ascending",
+//             "sSortDescending": " - click/return to sort descending"
+//         },
+//         "sLengthMenu": "显示 _MENU_ 记录",
+//         "sZeroRecords": "对不起，查询不到任何相关数据",
+//         "sEmptyTable": "未有相关数据",
+//         "sLoadingRecords": "正在加载数据-请等待...",
+//         "sInfo": "当前显示 _START_ 到 _END_ 条，共 _TOTAL_ 条记录。",
+//         "sInfoEmpty": "当前显示0到0条，共0条记录",
+//         "sInfoFiltered": "（数据库中共为 _MAX_ 条记录）",
+//         "sProcessing": "<img src='../resources/user_share/row_details/select2-spinner.gif'/> 正在加载数据...",
+//         "sSearch": "模糊查询：",
+//         "sUrl": "",
+//         //多语言配置文件，可将oLanguage的设置放在一个txt文件中，例：Javascript/datatable/dtCH.txt
+//         "oPaginate": {
+//             "sFirst": "首页",
+//             "sPrevious": " 上一页 ",
+//             "sNext": " 下一页 ",
+//             "sLast": " 尾页 "
+//         }
+//     },
+
+//     "columnDefs": [{
+//         orderable: false,
+
+//         targets: 0
+//     }, {
+//         orderable: false,
+
+//         targets: 1
+//     }], //第一列与第二列禁止排序
+
+//     "order": [
+//         [0, null]
+//     ], //第一列排序图标改为默认
+//     initComplete: function() { //列筛选
+//         var api = this.api();
+//         api.columns().indexes().flatten().each(function(i) {
+//             if (i != 0 && i != 1) { //删除第一列与第二列的筛选框
+//                 var column = api.column(i);
+//                 var $span = $('<span class="addselect">▾</span>').appendTo($(column.header()))
+//                 var select = $('<select><option value="">All</option></select>')
+//                     .appendTo($(column.header()))
+//                     .on('click', function(evt) {
+//                         evt.stopPropagation();
+//                         var val = $.fn.dataTable.util.escapeRegex(
+//                             $(this).val()
+//                         );
+//                         column
+//                             .search(val ? '^' + val + '$' : '', true, false)
+//                             .draw();
+//                     });
+//                 column.data().unique().sort().each(function(d, j) {
+//                     function delHtmlTag(str) {
+//                         return str.replace(/<[^>]+>/g, ""); //去掉html标签
+//                     }
+
+//                     d = delHtmlTag(d)
+//                     select.append('<option value="' + d + '">' + d + '</option>')
+//                     $span.append(select)
+//                 });
+
+//             }
+//         });
+
+//     }
+
+// });
 
 
 $('.responsive-data-table').DataTable({
@@ -44,4 +128,3 @@ $('.responsive-data-table').DataTable({
     responsive: true,
     dom: '<"tbl-top clearfix"lfr>,t,<"tbl-footer clearfix"<"tbl-info pull-left"i><"tbl-pagin pull-right"p>>'
 });
-
--- dl/dist/js/main.js
+++ dl/dist/js/main.js
@@ -3,18 +3,18 @@
  * Copyright 2017 ThemeBucket
  * Licensed under the ISC license
  */
-+function ($) {
++ function($) {
 
     'use strict';
 
-    $(function(){
+    $(function() {
 
-        $('[data-toggle="ui-nav"]').on('click', function(e){
+        $('[data-toggle="ui-nav"]').on('click', function(e) {
             e.preventDefault();
             $('#ui').toggleClass('ui-aside-compact');
         });
 
-        $('[data-toggle="ui-aside-right"]').on('click', function(e){
+        $('[data-toggle="ui-aside-right"]').on('click', function(e) {
             e.preventDefault();
             $('#content').toggleClass('ui-content-compact');
         });
@@ -24,15 +24,16 @@
             // locate href
             // if there is no submenu
             var href = $(this).attr('href');
-            if(href){
+            if (href) {
                 window.location.href = href;
             }
 
             // Open submenu
-            var $this = $(e.target), $active;
+            var $this = $(e.target),
+                $active;
             $this.is('a') || ($this = $this.closest('a'));
 
-            $active = $this.parent().siblings( ".active" );
+            $active = $this.parent().siblings(".active");
             $active && $active.toggleClass('active').find('> ul:visible').stop().slideUp(200);
 
             ($this.parent().hasClass('active') && $this.next().stop().slideUp(200)) || $this.next().stop().slideDown(200);
@@ -41,10 +42,10 @@
             $this.next().is('ul') && e.preventDefault();
         });
 
-        if($('#ui').hasClass('ui-aside-compact')) {
+        if ($('#ui').hasClass('ui-aside-compact')) {
             var uiHasCompact = true;
         }
-        if($('#content').hasClass('ui-content-compact')) {
+        if ($('#content').hasClass('ui-content-compact')) {
             var uiContentHasCompact = true;
         }
 
@@ -52,46 +53,46 @@
             if (Modernizr.mq('screen and (min-width:768px)')) {
                 // action for screen widths including and above 768 pixels
 
-                if(uiHasCompact === true) {
+                if (uiHasCompact === true) {
                     $('#ui').addClass('ui-aside-compact')
                 }
-                if(uiContentHasCompact === true) {
+                if (uiContentHasCompact === true) {
                     $('#content').addClass('ui-content-compact')
                 }
 
             } else if (Modernizr.mq('screen and (max-width:767px)')) {
                 // action for screen widths below 768 pixels
-                // console.log('Moblie');
+                // ('Moblie');
 
-                if(uiHasCompact === true) {
+                if (uiHasCompact === true) {
                     $('#ui').removeClass('ui-aside-compact')
                 }
-                if(uiContentHasCompact === true) {
+                if (uiContentHasCompact === true) {
                     $('#content').removeClass('ui-content-compact')
                 }
 
-                $('[data-toggle="ui-nav"]').on('click', function(e){
+                $('[data-toggle="ui-nav"]').on('click', function(e) {
                     e.preventDefault();
 
                     var hasAsideCompact = $('#ui').hasClass('ui-aside-compact'),
-                    hasContentCompact = $('#content').hasClass('ui-content-compact');
+                        hasContentCompact = $('#content').hasClass('ui-content-compact');
 
-                    if(hasAsideCompact) {
-                        if(hasContentCompact) {
+                    if (hasAsideCompact) {
+                        if (hasContentCompact) {
                             $('#content').removeClass('ui-content-compact');
                         }
                     }
 
                 });
 
-                $('[data-toggle="ui-aside-right"]').on('click', function(e){
+                $('[data-toggle="ui-aside-right"]').on('click', function(e) {
                     e.preventDefault();
 
                     var hasAsideCompact = $('#ui').hasClass('ui-aside-compact'),
-                    hasContentCompact = $('#content').hasClass('ui-content-compact');
+                        hasContentCompact = $('#content').hasClass('ui-content-compact');
 
-                    if(hasContentCompact) {
-                        if(hasAsideCompact) {
+                    if (hasContentCompact) {
+                        if (hasAsideCompact) {
                             $('#ui').removeClass('ui-aside-compact');
                         }
                     }
@@ -100,7 +101,7 @@
         }
 
         var id;
-        $(window).resize(function () {
+        $(window).resize(function() {
             clearTimeout(id);
             id = setTimeout(doneResizing, 0);
         });
@@ -108,71 +109,198 @@
         doneResizing();
 
     });
-    
+
 }(jQuery);
 
 (function($) {
-
+    
     'use strict';
 
     $(document).ready(function() {
+
+        var fetchData = function () {
+            var dataTime, groupList;
+            var colorArr = ['#939393', '#1afa29', '#FF0', '#F00', '#939393']
+            var docfrag = document.createDocumentFragment();
+            var getUsedPct = function(hard) {
+                return !!hard ? (hard.UsedPct === 4 ? '? %' : hard.UsedPct + ' %') : '';
+            }
+            var createDiv = function(ctn, name, className) {
+                var DIV = document.createElement('div');
+                DIV.textContent = ctn || '';
+                DIV.className = !!className && 'st-' + className;
+                if (name === 'disk ' && !!ctn) {
+                    var PIE = document.createElement('div');
+                    PIE.className = 'pie';
+                    PIE.textContent = ctn.slice(2, -2);
+                    PIE.style.backgroundImage = 'linear-gradient(to right, transparent 50%, ' + colorArr[className] + ' 0)'
+                    var PIE_CHILD = document.createElement('div');
+                    PIE_CHILD.className = 'pie-child';
+                    PIE_CHILD.style.animation = "spin 50s linear infinite, st-" + className + " 100s step-end infinite";
+                    PIE_CHILD.style.animationDelay = 'inherit';
+                    PIE_CHILD.style.animationPlayState = 'paused';
+                    PIE.appendChild(PIE_CHILD);
+                    DIV.appendChild(PIE);
+                } else {
+                    var DIV_STATE = document.createElement('div');
+                    DIV_STATE.className = 'state-point';
+                    DIV_STATE.style.backgroundColor = colorArr[className]
+                    DIV.insertBefore(DIV_STATE, DIV.childNodes[0]);
+                }
+                return DIV;
+            }
+            var createTd = function(ctn, name, className) {
+                var TD = document.createElement('td');
+                TD.className = name + (!!className && 'st-' + className);
+                TD.textContent = ctn || '';
+                if (['cpu ', 'ram ', 'disk '].indexOf(name) !== -1 && !!ctn) {
+                    var DIV = document.createElement('div');
+                    DIV.className = 'pie';
+                    DIV.textContent = ctn.slice(0, -2);
+                    DIV.style.backgroundImage = 'linear-gradient(to right, transparent 50%, ' + colorArr[className] + ' 0)'
+                    var DIV_CHILD = document.createElement('div');
+                    DIV_CHILD.className = 'pie-child';
+                    DIV_CHILD.style.animation = "spin 50s linear infinite, st-" + className + " 100s step-end infinite";
+                    DIV_CHILD.style.animationDelay = 'inherit';
+                    DIV_CHILD.style.animationPlayState = 'paused';
+                    DIV.appendChild(DIV_CHILD);
+                    TD.appendChild(DIV);
+                } else if(name === 'name ') {
+                    var DIV = document.createElement('div');
+                    DIV.className = 'state-point';
+                    DIV.style.backgroundColor = colorArr[className]
+                    TD.insertBefore(DIV, TD.childNodes[0]);
+                }
+                return TD;
+            };
+            var createTdRowspan = function(ctn, rowspan) {
+                var TD = document.createElement('td');
+                TD.textContent = ctn || '';
+                if (Object.prototype.toString.call(rowspan * 1) === '[object Number]') {
+                    TD.setAttribute('rowspan', rowspan)
+                }
+                return TD;
+            };
+            var createUnit = function(group, docfrag) {
+                var tr = document.createElement('tr');
+                var td;
+                var tdFirst = createTdRowspan(group.GroupName, group.MoList.length);
+                tr.appendChild(tdFirst);
+                group.MoList.forEach(function(item, i) {
+                    tr.append(createTd(item.MODisplayName, 'name ', item.MoState));
+                    tr.append(createTd(getUsedPct(item.CPU), 'cpu ', (item.CPU && item.CPU.State)));
+                    tr.append(createTd(getUsedPct(item.RAM), 'ram ', (item.RAM && item.RAM.State)));
+
+                    td = document.createElement('td');
+                    if (!!item.DiskList) {
+                        item.DiskList.forEach(function(item, i) {
+                            td.append(createDiv(item.Name + getUsedPct(item), 'disk ', item.State))
+                        })
+                    } else {
+                        td.textContent = "";
+                    }
+                    td.className = 'disk';
+                    tr.append(td);
+
+                    td = document.createElement('td');
+                    if (!!item.MPList) {
+                        item.MPList.forEach(function(item, i) {
+                            td.append(createDiv(item.DisplayName, 'mp ', item.State))
+                        });
+                    } else {
+                        td.textContent = "";
+                    }
+                    td.className = 'cp'
+                    tr.append(td);
+
+                    docfrag.appendChild(tr);
+                    tr = document.createElement('tr');
+                })
+                return docfrag;
+            }
+            var checkEmptyElement = function (eleGroup) {
+                var empty = 0;
+                var len = eleGroup.length;
+                for (var i = 0; i < len; i++) {
+                    if(!!eleGroup[i].innerHTML) return false;
+                }
+                return true;
+            }
+            $("<div class='refresh-block'><span class='refresh-loader'><i class='fa fa-spinner fa-spin'></i></span></div>").appendTo($(this).parents('.tools').parents('.panel-heading').parents('.panel'));
+            $.ajax({
+                url: 'ex.json',
+                method: 'get',
+                success: function(res) {
+                    console.log(res);
+                    var disappear = [];
+                    dataTime = res.DataTime;
+                    groupList = res.GroupList;
+
+                    groupList.forEach(function(item) {
+                        docfrag = createUnit(item, docfrag);
+                    })
+                    if(checkEmptyElement(docfrag.querySelectorAll('.name'))) {
+                        disappear.push('.name')
+                    };
+                    if(checkEmptyElement(docfrag.querySelectorAll('.cpu'))) {
+                        disappear.push('.cpu')
+                    };
+                    if(checkEmptyElement(docfrag.querySelectorAll('.ram'))) {
+                        disappear.push('.ram')
+                    };
+                    if(checkEmptyElement(docfrag.querySelectorAll('.disk'))) {
+                        disappear.push('.disk')
+                    };
+                    if(checkEmptyElement(docfrag.querySelectorAll('.cp'))) {
+                        disappear.push('.cp')
+                    };
+                    $("tbody tr").remove();
+                    $('tbody').append(docfrag);
+                    disappear.forEach(function (item) {
+                        $(item).css('display', 'none');
+                    })
+                    document.querySelectorAll('.pie').forEach(function(pie) {
+                        console.log(pie);
+                        var p = parseFloat(pie.textContent);
+                        console.log(pie.textContent, pie.textContent.length);
+                        pie.style.animationDelay = '-' + p + 's';
+                    });
+                    $('.update-time').html('<span>数据更新点：</span> ' + new Date(dataTime.slice(7, -2) * 1).toLocaleString());
+                    $('.refresh-block').remove();
+                },
+                error: function(err) {
+                    console.log(err);
+                }
+            })
+        }
+
+        fetchData();
 
         $('[data-toggle="tooltip"]').tooltip();
         $('[data-toggle="popover"]').popover();
 
         // collapsible panel
-        $('.panel .tools .collapse-box').click(function () {
+        $('.panel .tools .collapse-box').click(function() {
             var el = $(this).parents(".panel").children(".panel-body");
             if ($(this).hasClass("fa-chevron-down")) {
                 $(this).removeClass("fa-chevron-down").addClass("fa-chevron-up");
                 el.slideUp(200);
             } else {
                 $(this).removeClass("fa-chevron-up").addClass("fa-chevron-down");
-                el.slideDown(200); }
+                el.slideDown(200);
+            }
         });
 
         // close panel
-        $('.panel .tools .close-box').click(function () {
+        $('.panel .tools .close-box').click(function() {
             $(this).parents(".panel").parent().remove();
         });
 
-// refresh panel
-
-$('.refresh-box').on('click', function(br) {
-    br.preventDefault();
-    $("<div class='refresh-block'><span class='refresh-loader'><i class='fa fa-spinner fa-spin'></i></span></div>").appendTo($(this).parents('.tools').parents('.panel-heading').parents('.panel'));
-
-    $.ajax({
-        url: 'ex.json',
-        method: 'get',
-        success: function (res) {
-            console.log(res);
-
-
-
-
-
-
-
-
-
-
-
-
-            $('.refresh-block').remove();
-        },
-        error: function (err) {
-            console.log(err);
-        }
-    })
-
-    var arr = [[1,2,3,4,5,6,7], ['X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7'], [11,22,33,44,55,66,77]]
-    
-    arr.forEach(function (item, i) {
-        // var td = d3.select('tbody').data(arr[i]).text(function (d) { return ''; });
-        // var tr = td.enter().append('tr').text(function(d) { return '<td><'+d+'td>'; });
-    })
-});
+        // refresh panel
+        $('.refresh-box').on('click', function(br) {
+            br.preventDefault();
+            fetchData.call(this);
+        });
 
         // Auto size textarea
         autosize($('.autosize'));
@@ -180,7 +308,7 @@ $('.refresh-box').on('click', function(br) {
         // searchCollapse
         $('.search-collapse').each(function() {
             var $searchInput = $('.form-control'),
-            $searchBtn = $('.btn-search-collapse');
+                $searchBtn = $('.btn-search-collapse');
 
             $searchBtn.click(function(e) {
                 e.preventDefault();
@@ -191,12 +319,12 @@ $('.refresh-box').on('click', function(br) {
 
         // niceScroll
         $(".niceScroll").niceScroll({
-            cursorcolor : "#000000",
-            cursoropacitymax : 0.3,
-            cursorwidth : "8px",
-            cursorborder : "2px solid transparent",
-            cursordragontouch : true, // drag cursor in touch / touchbehavior mode also
-            zindex : "10", // change z-index for scrollbar div
+            cursorcolor: "#000000",
+            cursoropacitymax: 0.3,
+            cursorwidth: "8px",
+            cursorborder: "2px solid transparent",
+            cursordragontouch: true, // drag cursor in touch / touchbehavior mode also
+            zindex: "10", // change z-index for scrollbar div
             cursorborderradius: "10px",
         });
 
@@ -207,6 +335,8 @@ $('.refresh-box').on('click', function(br) {
 (function($) {
 
     $(document).ready(function() {
+
+        
 
         'use strict';
 
@@ -274,41 +404,43 @@ $('.refresh-box').on('click', function(br) {
 })(window.jQuery);
 
 (function($) {
-    
+
     $(document).ready(function() {
 
-    'use strict';
+        
 
-    // Return to Mail-list
-    // when click .return-mail-list > a
-    $('.return-note-list').click(function(e) {
-        e.preventDefault();
-        $('.Note-aside').removeClass('Note-aside--slideleft');
-    });
+        'use strict';
 
-    if ($(window).width() <= 767) {
-        $('.Note-list').on('click', '.Note', function(e){
+        // Return to Mail-list
+        // when click .return-mail-list > a
+        $('.return-note-list').click(function(e) {
             e.preventDefault();
-            $('.Note-aside').addClass('Note-aside--slideleft');
+            $('.Note-aside').removeClass('Note-aside--slideleft');
         });
-    };
 
-
-    $(window).resize(function() {
         if ($(window).width() <= 767) {
-            $('.Note-list').on('click', '.Note', function(e){
+            $('.Note-list').on('click', '.Note', function(e) {
                 e.preventDefault();
                 $('.Note-aside').addClass('Note-aside--slideleft');
             });
-        } else {
-            $('.Note-aside').removeClass('Note-aside--slideleft');
-            $('.Note-list').on('click', '.Note', function(e){
-                e.preventDefault();
-                $('.Note-aside').removeClass('Note-aside--slideleft');
-            });
-        }
-    });
+        };
 
-});
+
+        $(window).resize(function() {
+            if ($(window).width() <= 767) {
+                $('.Note-list').on('click', '.Note', function(e) {
+                    e.preventDefault();
+                    $('.Note-aside').addClass('Note-aside--slideleft');
+                });
+            } else {
+                $('.Note-aside').removeClass('Note-aside--slideleft');
+                $('.Note-list').on('click', '.Note', function(e) {
+                    e.preventDefault();
+                    $('.Note-aside').removeClass('Note-aside--slideleft');
+                });
+            }
+        });
+
+    });
 
 })(window.jQuery);
